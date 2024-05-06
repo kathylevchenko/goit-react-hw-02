a6da72fbb387af6a4f4f7bfcacdbf894584aada8
Kateryna Davidovych <kathylevchenko1@gmail.com>
2024-05-06 22:58:18 +0300

    add steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,56 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification"
+
+const App = () => {
+  const startFeedback = { good: 0, neutral: 0, bad: 0 };
+
+  const [state, setState] = useState(() => {
+    const storageFeedback = window.localStorage.getItem("saveFeedback");
+    return storageFeedback !== null
+      ? JSON.parse(storageFeedback)
+      : startFeedback;
+  });
+
+  const feedbackReset = () => {
+    setState(startFeedback);
+  };
+  useEffect(() => {
+    window.localStorage.setItem("saveFeedback", JSON.stringify(state));
+  }, [state]);
+
+  let totalFeedback = 0;
+
+  const updateFeedback = (feedbackType) => {
+    setState({
+      ...state,
+      [feedbackType]: state[feedbackType] + 1,
+    });
+  };
+
+  for (let i of Object.values(state)) {
+    totalFeedback += i;
+  }
 
   return (
     <>
-      <h1>
-      Sip Happens Café
-      </h1>
-      <div className="card">
-        <p>
-        Please leave your feedback about our service by selecting one of the options below.
-        </p>
-      </div>
-    </>
-  )
-}
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        feedbackReset={feedbackReset}
+      />
 
-export default App
+      {totalFeedback > 0 ? (
+        <Feedback feedback={state} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+};
+
+export default App;
